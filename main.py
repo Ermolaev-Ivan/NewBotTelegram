755bf8efc955d8a8a4cfccdc0d131895092e3064
Ermolaev-Ivan <thedollmaster@bk.ru>
2021-09-13 23:23:57 +0300

    second bot from video

--- main.py
+++ main.py
@@ -4,28 +4,30 @@ import telegram
 # подсчитывать их по команде, так же необходимо реалиазовать сброс каунта каждый день в 00:00 минут по московскому
 # времени(+3)
 
+def main():
+    TOKEN = open("TOKEN.txt").read()
+    bot = telegram.Bot(token=TOKEN)
+    # проверяем работу бота
+    print(bot.get_me())
 
-TOKEN = open("TOKEN.txt").read()
-bot = telegram.Bot(token=TOKEN)
-# проверяем работу бота
-print(bot.get_me())
+
+    # получаем обновления от бота
+    updates = bot.get_updates()
+
+    count = 0
+    # переделать список users на словарь, где будет значание username : кол-во плюсов,
+    # и переделать с этими условиями весь цикл
+    users = []
+
+    for upd in updates:
+        print(upd.message.text)
+        if "+" in upd.message.text and upd.message.chat.username not in users:
+            count += upd.message.text.count("+")
+            users.append(upd.message.chat.username)
+        else:
+            pass
+
+    print(count)
 
 
-# получаем обновления от бота
-updates = bot.get_updates()
-
-count = 0
-# переделать список users на словарь, где будет значание username : кол-во плюсов,
-# и переделать с этими условиями весь цикл
-users = []
-
-for upd in updates:
-    print(upd.message.text)
-    if "+" in upd.message.text and upd.message.chat.username not in users:
-        count += upd.message.text.count("+")
-        users.append(upd.message.chat.username)
-    else:
-        pass
-
-print(count)
 

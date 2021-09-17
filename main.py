f0f4b46bf2543aa1b81b5200862c1fd3e84ee970
Ivan Ermolaev <ermolaev_i@garpix.com>
2021-09-17 20:14:27 +0300

    commit

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ async def plusBot(message: types.Message):
     global count
     global sportsmans
     global day
+    name_sportsman = message.from_user.last_name + " " + message.from_user.first_name
     # определяем в какой день сообщение отправлено
     message_day = message.date.strftime('%d')
     # механизм скидывания счетчика когда наступает новый день
@@ -37,12 +38,21 @@ async def plusBot(message: types.Message):
     # если сообщение содержит "+" то происходит магия, а так же проверка чата, чтобы исключить обращения к боту в личку
     if message.text.find("+") != -1 and message.chat.id == CHAT:
         count = count + message.text.count("+")
-        name_sportsman = message.from_user.last_name + " " + message.from_user.first_name
         # Проверка,содержится ли плюсанувший челоек уже в словаре
         if name_sportsman in sportsmans.keys():
             sportsmans[name_sportsman] = sportsmans[name_sportsman] + message.text.count("+")
         else:
             sportsmans[name_sportsman] = message.text.count("+")
+    # elif message.text.replace(" ", "") == "-" and name_sportsman in sportsmans.keys():
+    #     if sportsmans[name_sportsman] == 1:
+    #         sportsmans.pop(name_sportsman)
+    #     else:
+    #         sportsmans[name_sportsman] = sportsmans[name_sportsman] - 1
+
+            # r"/.*(-).*/Ug"
+            # ".*(-).*"
+
+
 
     # Команда выводящая количество идущих
     if message.text == "/get":
@@ -55,7 +65,7 @@ async def plusBot(message: types.Message):
         if count == 0:
             await message.answer("На тренировку пока никто не собирается")
         else:
-            await  message.answer("На тренировку пойдут:")
+            await message.answer("На тренировку пойдут:")
             for key in sportsmans.keys():
                 if sportsmans[key] > 1:
                     await message.answer(f"{key} и приведет с собой еще {sportsmans[key] - 1}{ending2(sportsmans[key] - 1)}")
